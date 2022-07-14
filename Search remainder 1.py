55d313aa2f6c70926f57df87eb457477e9a49e46
minsing-jin <60510718+minsing-jin@users.noreply.github.com>
2022-07-14 22:51:08 +0900

    Create Search remainder 1.py

---
+++ Search remainder 1.py
@@ -0,0 +1,10 @@
+# my sol
+def solution(n):
+    for i in range(1,n+1):
+        if n%i == 1:
+         return i
+         
+         
+# other people sol
+def solution(n):
+    return [x for x in range(1,n+1) if n%x==1][0]
