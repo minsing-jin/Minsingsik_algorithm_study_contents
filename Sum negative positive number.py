3bd142a4623f3dc4b308abf30fe3aa0116f84bad
minsing-jin <60510718+minsing-jin@users.noreply.github.com>
2022-07-19 23:17:35 +0900

    Update Sum negative positive number.py

--- Sum negative positive number.py
+++ Sum negative positive number.py
@@ -13,3 +13,5 @@ def solution(absolutes, signs):
 # other sol
 def solution(absolutes, signs):
     return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
+
+# [ 조건 만족 시 출력값 if 조건 else 조건 불만족 시 출력 값 for i in data] 
