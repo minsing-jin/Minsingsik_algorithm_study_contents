74a5923e1c3a07e982ad0fff3fb5e5fa9ceca110
minsing-jin <minsung0722@khu.ac.kr>
2022-10-02 16:40:09 +0900

    Update and rename programmers/실패 문제들/2016년.py to programmers/2016년(3번만에 맞음!).py

--- programmers/2016년(3번만에 맞음!).py
+++ programmers/2016년(3번만에 맞음!).py
@@ -1,6 +1,20 @@
-# 졌다....
+# 해결!
 
-# 틀린풀이
+# my sol
+
+def solution(a, b):
+    
+    n = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    D = ['THU','FRI','SAT','SUN', 'MON','TUE','WED']
+    if a == 1:
+        return D[b%7]
+    else:
+        
+        return  D[((sum(n[:a])+b)%7)] 
+
+
+# 틀린풀이 -> line 11에 index out of range와 더불어 n에서 a의 당월은 31일같은 일수를 세면 안돼는데 세서 실패했었음!
+
 def solution(a, b):
     
     n = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
