08cc2bb5ebf8ff806193e313bf6e8c607a82a161
minsing-jin <minsung0722@khu.ac.kr>
2022-10-02 15:05:34 +0900

    Update Secret_map.py

--- programmers/Secret_map.py
+++ programmers/Secret_map.py
@@ -70,7 +70,7 @@ def solution(n, arr1, arr2):
 
   
 # other sol
-
+# 1
 def solution(n, arr1, arr2):
     answer = []
     for i,j in zip(arr1,arr2):
@@ -80,3 +80,8 @@ def solution(n, arr1, arr2):
         a12=a12.replace('0',' ')
         answer.append(a12)
     return answer
+
+
+
+# 2
+solution = lambda n, arr1, arr2: ([''.join(map(lambda x: '#' if x=='1' else ' ', "{0:b}".format(row).zfill(n))) for row in (a|b for a, b in zip(arr1, arr2))])
