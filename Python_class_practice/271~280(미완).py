9667af4799c558fe1d670e071f3108f8ad6c48a6
minsing-jin <minsung0722@khu.ac.kr>
2022-11-29 22:56:31 +0900

    Update 271~280(미완).py

--- Python_class_practice/271~280(미완).py
+++ Python_class_practice/271~280(미완).py
@@ -6,6 +6,9 @@ class Account:
     acc_cnt = 0
 
     def __init__(self, account_H, balanced):        # 생성자(constructor): 맨처음 초기 생성
+        self.deposit_count = 0
+        self.deposit_lst = []
+        self.withdraw_lst = []
         self.account_H = account_H
         self.balanced = balanced
         
@@ -31,18 +34,28 @@ class Account:
       print(cls.acc_cnt)          # 클래스 변수:  클래스 내부에 선언된 변수, 일반적인 변수와 동일한 형태
 
 
-    def deposit(self,deposit_won):
-      if deposit_won >= 1:
-        if Account.acc_cnt == 5:
-          self.balanced += deposit_won
-          self.balanced *= 1.01
 
-        else:
-          self.balanced += deposit_won   # self 주소의 balanced의 주소에 계속해서 입금 금액 더하기
-      Account.acc_cnt = 0
+
+
+
+
+    def deposit(self, amount):
+      self.deposit_lst.append(amount)
+      if amount >= 1:
+          self.balanced += amount     # self 주소의 balanced의 주소에 계속해서 입금 금액 더하기
+
+          self.deposit_count += 1
+
+          if self.deposit_count % 5 == 0:         # 5, 10, 15
+              # 이자 지금
+              self.balanced = (self.balanced * 1.01)
+
+
 
 
     def withdraw(self, withdraw_won):       # self 주소의 withdraw_won 의 주소에 계속해서 출금 금액 빼기 / construtor에 있는 변수들을 사용할 경우에는 global variable이 아니므로 주소로 접근해야함. 따라서 self를 붙여서 접근
+      self.withdraw_lst.append(withdraw_won)
+      self.withdraw_won = withdraw_won   # 출금 내역
       if self.balanced >= withdraw_won:      # 따라서 메서드를 인스턴스 메서드는 인스턴스 필드(클래스에 저장되어있는 변수) 영역으로 진입할 수 있음. 클래스에 저장되어있는 변수들을 마음껏 사용할 수 있다. 그래서 처음에 self를 붙이는 거임. 인스턴스 메서드의 인스턴스 필드의 클래스 변수들을 사용하려고!
         self.balanced -= withdraw_won
       else:
@@ -76,7 +89,28 @@ class Account:
       #     break
       # print('잔고: ' + tmp + '원')
 
+    def deposit_history(self):
+      print('입금내역: ',  end = '\n')
+      for i in self.deposit_lst:
+        print(i)
 
+    
+    def withdraw_history (self):
+      print('출금내역: ', end = '\n')
+      for i in self.withdraw_lst:
+        print(i)
+
+
+#  279번 객체 순회로 잔고 100만원 이상이 고객 정보 출력
+# 278번 여러 객체 생성 -> account 클래스로 부터 3개 이상의 인스턴스 생성, 인스턴스를 리스트에 저장
+k = Account("kim", 10000000)
+t = Account("ted", 10000)
+n = Account("nod", 10000)
+lst = [k,t,n]
+
+for i in lst:
+  if i.balanced >= 1000000:
+    i.display_info()
 
 
 
@@ -105,17 +139,39 @@ k.deposit(100)
 k.withdraw(90)
 '''
 
-# k = Account("kim", 100)
-# k.deposit(100)
-# k.withdraw(90)
-# k.display_info()
+'''
+276-276
+k = Account("kim", 100)
+k.deposit(100)
+k.withdraw(90)
+k.display_info()
 
 p = Account("파이썬", 1000)
 p.display_info()
+'''
+
+
+'''
+277번 이자 붙이기
+p = Account("파이썬", 10000)
+p.deposit(10000)
+p.deposit(10000)
+p.deposit(10000)
+p.deposit(5000)
+p.deposit(5000)
+print(p.balanced)
+
+'''
 
 
 
-
-
-
-# 277 번부터 시작 이자 붙이는 코드 구현 할것
+'''
+280 번 입출금 내역 출력
+k = Account("kim", 100)
+k.deposit(100)
+k.withdraw(90)
+k.deposit(100)
+k.withdraw(90)
+k.deposit_history()
+k.withdraw_history()
+'''
