053194f8655eb0bddbc1aa2b1e56995917bb1ae0
minsing-jin <minsung0722@khu.ac.kr>
2022-12-11 23:35:58 +0900

    Update 시저암호.py

--- programmers/실패 문제들/시저암호.py
+++ programmers/실패 문제들/시저암호.py
@@ -1,3 +1,27 @@
+# my sol
+def solution(s, n):
+
+    A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    tmp = [i for i in s]
+
+    for i in range(len(tmp)):
+        if tmp[i].isalpha() == True:
+            A.find(s[i])
+            a.find(s[i])
+        if A.find(s[i]) != -1:
+
+            tmp[i] = A[((A.find(s[i])+n)%len(A))]
+
+        if a.find(s[i]) != -1:
+
+            tmp[i] = a[((a.find(s[i])+n)%len(a))]
+
+
+    return ''.join(tmp)
+
+
+# other sol
 def caesar(s, n):
     s = list(s)
     for i in range(len(s)):
@@ -5,7 +29,16 @@ def caesar(s, n):
             s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
         elif s[i].islower():
             s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
- 
+
     return "".join(s)
- 
-print(caesar("a B z", 4)) # 'e F d'
+    # 주어진 문장을 암호화하여 반환하세요.
+
+
+# 실행을 위한 테스트코드입니다.
+print('s는 "a B z", n은 4인 경우: ' + caesar("a B z", 4))
+
+
+
+
+def solution(s, n):
+    return ''.join([chr(ord(i) + (not ord(i)==32)*((n%26) -26*((90<(ord(i)+(n%26))*(ord(i)<91)) + (122<(ord(i)+(n%26)))))) for i in s])
