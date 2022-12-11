b314a51362d31397dd3c5c02eb26d5bf2dfb2fb1
minsing-jin <minsung0722@khu.ac.kr>
2022-12-11 23:40:43 +0900

    Update and rename 신고결과 받기.py to Get_report_result.py

--- programmers/실패 문제들/Get_report_result.py
+++ programmers/실패 문제들/Get_report_result.py
@@ -1,5 +1,6 @@
+# my sol
 def solution(id_list, report, k):
-
+    
     report = set(report)  # 중복조건 빼주기!!!
 
     report_list = []
@@ -15,5 +16,22 @@ def solution(id_list, report, k):
             answer[n.split()[0]] += 1
 
 
-
+    
     return list(answer.values())
+
+
+
+# other sol
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
