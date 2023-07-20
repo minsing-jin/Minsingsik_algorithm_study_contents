0fc916612abec12c168e38850a3334f1515edcc2
minsing-jin <minsung0722@khu.ac.kr>
2023-07-20 23:43:02 +0900

    Create 기사단원의 무기(실패).py

---
+++ programmers/기사단원의 무기(실패).py
@@ -0,0 +1,38 @@
+def solution(number, limit, power):
+    tmp = [1]
+    cnt = 0
+    if number > 1:
+      for n in range(2,number+1):
+          print("n: ", n)
+
+          for i in range(1, (n//2)+1):
+              
+              if n%i == 0:
+                  cnt += 1
+          print("before cnt: ", cnt)
+          
+
+
+
+          if cnt % 2 == 0:
+            
+            cnt = (cnt*2)-1
+
+          else:
+            cnt = cnt *2
+          print("cnt: ", cnt)
+          
+
+
+
+
+          if cnt > limit:
+            tmp.append(power)
+          else:
+            tmp.append(cnt)  
+          cnt = 0
+          
+          print("tmp: ", tmp)
+
+          
+    return sum(tmp)
