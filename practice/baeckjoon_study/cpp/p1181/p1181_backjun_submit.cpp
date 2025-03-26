9cacb5d213944b9cd15005bc5cd2da49023bbdb6
minsing-jin <ironman0722@naver.com>
2025-03-26 17:46:44 +0900

    Improve my backjoon code.

--- practice/baeckjoon_study/cpp/p1181/p1181_backjun_submit.cpp
+++ practice/baeckjoon_study/cpp/p1181/p1181_backjun_submit.cpp
@@ -4,80 +4,38 @@
 #include "iostream"
 #include "string"
 #include "vector"
-#include <cctype>
 #include <algorithm>
-#include <sstream>
 
 using namespace std;
 
-void sort_word_min_selection(vector<string> &word_lst){
-    int min_length;
-    int iter = 0;
-    int min_idx;
-    string swapping_temp;
-
-    while (iter < word_lst.size()) {
-        min_length = word_lst[iter].length();
-        min_idx = iter;
-
-        // 길이순 정렬
-        for (int i = iter; i < word_lst.size(); i++) {
-            if (min_length > word_lst[i].length()) {
-                min_length = word_lst[i].length();
-                min_idx = i;
-            }
-        }
-
-        swapping_temp = word_lst[iter];
-        word_lst[iter] = word_lst[min_idx];
-        word_lst[min_idx] = swapping_temp;
-
-        // initialize_idx
-        iter++;
+bool compare(string &a, string &b){
+    if (a.length() == b.length()){
+        return a < b;
     }
-    // lexicographical sorting
-    for (int i = 0; i < word_lst.size()-1; i++) {
-        // ist word lexicographical sorting.
-        for (int j = i+1; j < word_lst.size(); j++){
-            if (word_lst[j-1].length() == word_lst[j].length()){
-                if (word_lst[j-1] > word_lst[j]){
-                    swapping_temp = word_lst[j-1];
-                    word_lst[j-1] = word_lst[j];
-                    word_lst[j] = swapping_temp;
-                }
-            }
-        }
+    else{
+        return a.length() < b.length();
     }
 }
 
 int main() {
     string input_word;
     vector<string> word_lst;
+    int N;
 
-    while (std::getline(std::cin, input_word)) {
-        bool digit_flag = false;
-        for (char character: input_word) {
-            if (isdigit(character)) {
-                digit_flag = true;
-                break;
-            }
-        }
-        if (!digit_flag)
-            word_lst.push_back(input_word);
+    cin >> N;
 
-        if (input_word.empty()) break;
+    for (int i = 0; i < N; i++) {
+        cin >> input_word;
+        word_lst.push_back(input_word);
     }
+
+
+    sort(word_lst.begin(), word_lst.end(), compare);
 
     for (int i = 0; i < word_lst.size(); i++) {
-        for (int j = i + 1; j < word_lst.size(); j++) {
-            if (word_lst[i] == word_lst[j]) {
-                word_lst.erase(word_lst.begin() + j);
-                j--;
-            }
+        if (word_lst[i] == word_lst[i - 1]) {	// 같은 단어가 여러 번 입력된 경우에는 한 번씩만 출력
+            continue;
         }
+        cout << word_lst[i] << "\n";
     }
-
-    sort_word_min_selection(word_lst);
-    for (string word: word_lst)
-        cout << word << endl;
 }
